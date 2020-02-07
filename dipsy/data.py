22c5e84e117336172c9ce24fefd53460c8563daa
Til Birnstiel <birnstiel@me.com>
2020-02-07 13:50:00 +0100

    added testi dataset

--- dipsy/data.py
+++ dipsy/data.py
@@ -6,6 +6,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+class Testi2014(object):
+
+    F_mm = None
+    alpha_mm = None
+    d_pc = None
+    region = None
+
+    def __init__(self):
+        """
+        Data from Testi et al. 2014 containing
+
+        F_mm : array
+            flux at 1 mm [Jy]
+
+        alpha_mm : array
+            spectral index between 1 and 3 mm
+
+        region : list
+            star forming region
+
+        d_pc : array
+            distance in parsec
+        """
+        fname = pkg_resources.resource_filename(__name__, os.path.join('datasets', 'testi2014.csv'))
+
+        self.F_mm, self.alpha_mm, self.d_pc = \
+            np.loadtxt(fname, skiprows=1, delimiter=',', usecols=[0, 1, 3]).T
+
+        with open(fname) as fid:
+            _ = fid.readline()
+            self.region = [line.split('"')[1] for line in fid.readlines()]
+
+    def plot(self):
+        """
+        Creates a plot of the flux-alpha observations
+        Returns the figure handle and axis handle.
+        """
+
+        f, ax = plt.subplots()
+
+        ax.scatter(self.F_mm, self.alpha_mm, c='k')
+        ax.set_xlim(10, 1000)
+        ax.set_xscale('log')
+        ax.set_ylim(1.5, 4.5)
+        ax.set_xlabel(r'$F_\mathrm{mm}$ [Jy]')
+        ax.set_ylabel(r'$\alpha_\mathrm{1-3mm}$')
+        return f, ax
+
+
+
 class Tripathi2017(object):
 
     R_eff = None
